d79afff281e696afb9f6e9f469b8be9b0acb1ad6
Michael J. Forster <mike@forsterfamily.ca>
2017-01-13 13:56:16 -0600

    Refactor FEATURE-TO-SITE

--- static/mhs-map/js/map.js
+++ static/mhs-map/js/map.js
@@ -24,16 +24,25 @@ function Site(sNo, sName, mName, sAddress, stName, sUrl, latLng) {
     this.latLng = latLng;
     return this;
 };
+function geometryPointX(geometry) {
+    if (geometry.type !== 'Point') {
+        throw 'Geometry is not a Point';
+    } else {
+        return geometry.coordinates[0];
+    };
+};
+function geometryPointY(geometry) {
+    if (geometry.type !== 'Point') {
+        throw 'Geometry is not a Point';
+    } else {
+        return geometry.coordinates[1];
+    };
+};
+function geometryPointToLatLng(geometry) {
+    return new google.maps.LatLng({ 'lat' : geometryPointY(geometry), 'lng' : geometryPointX(geometry) });
+};
 function featureToSite(feature) {
-    var site = new Site();
-    site.sNo = feature.properties.sNo;
-    site.sName = feature.properties.sName;
-    site.mName = feature.properties.mName;
-    site.sAddress = feature.properties.sAddress;
-    site.stName = feature.properties.stName;
-    site.sUrl = feature.properties.sUrl;
-    site.latLng = new google.maps.LatLng({ 'lat' : feature.geometry.coordinates[1], 'lng' : feature.geometry.coordinates[0] });
-    return site;
+    return new Site(feature.properties.sNo, feature.properties.sName, feature.properties.mName, feature.properties.sAddress, feature.properties.stName, feature.properties.sUrl, geometryPointToLatLng(feature.geometry));
 };
 function Sites(url) {
     this.url = url;
@@ -80,20 +89,20 @@ function ListWidget(model, jqelement) {
     return this;
 };
 function siteIconUri(site) {
-    var stName483 = site.stName;
-    if (stName483 === 'Featured site') {
+    var stName10 = site.stName;
+    if (stName10 === 'Featured site') {
         return 'icon_feature.png';
-    } else if (stName483 === 'Museum/Archives') {
+    } else if (stName10 === 'Museum/Archives') {
         return 'icon_museum.png';
-    } else if (stName483 === 'Building') {
+    } else if (stName10 === 'Building') {
         return 'icon_building.png';
-    } else if (stName483 === 'Monument') {
+    } else if (stName10 === 'Monument') {
         return 'icon_monument.png';
-    } else if (stName483 === 'Cemetery') {
+    } else if (stName10 === 'Cemetery') {
         return 'icon_cemetery.png';
-    } else if (stName483 === 'Location') {
+    } else if (stName10 === 'Location') {
         return 'icon_location.png';
-    } else if (stName483 === 'Other') {
+    } else if (stName10 === 'Other') {
         return 'icon_other.png';
     };
 };
@@ -105,8 +114,8 @@ function siteMarkerIcon(site) {
            };
 };
 function siteLinkTitle(site) {
-    var sAddress484 = site.sAddress;
-    return site.sName + ', ' + site.mName + (sAddress484 === '' ? '' : ', ' + sAddress484);
+    var sAddress11 = site.sAddress;
+    return site.sName + ', ' + site.mName + (sAddress11 === '' ? '' : ', ' + sAddress11);
 };
 function siteLinkUrl(site) {
     return MHSBASEURI + site.sUrl;
@@ -133,13 +142,13 @@ function MapWidget(model, jqelement, center, zoom, geolocationOptions) {
     this.siteInfoWindow = new google.maps.InfoWindow({  });
     this.googleMap = new google.maps.Map(jqelement[0], { 'center' : center, 'zoom' : zoom });
     this.updateWidget = function () {
-        for (var marker = null, _js_arrvar488 = this.markers, _js_idx487 = 0; _js_idx487 < _js_arrvar488.length; _js_idx487 += 1) {
-            marker = _js_arrvar488[_js_idx487];
+        for (var marker = null, _js_arrvar15 = this.markers, _js_idx14 = 0; _js_idx14 < _js_arrvar15.length; _js_idx14 += 1) {
+            marker = _js_arrvar15[_js_idx14];
             marker.setMap(null);
         };
         this.markers.length = 0;
-        for (var site = null, _js_arrvar486 = this.model.sites, _js_idx485 = 0; _js_idx485 < _js_arrvar486.length; _js_idx485 += 1) {
-            site = _js_arrvar486[_js_idx485];
+        for (var site = null, _js_arrvar13 = this.model.sites, _js_idx12 = 0; _js_idx12 < _js_arrvar13.length; _js_idx12 += 1) {
+            site = _js_arrvar13[_js_idx12];
             var marker = mapAddMarker(this.googleMap, this.siteInfoWindow, site);
             this.markers.push(marker);
         };
@@ -148,25 +157,25 @@ function MapWidget(model, jqelement, center, zoom, geolocationOptions) {
 };
 function mapWidgetStartUpdatingMarkers(mapWidget) {
     return mapWidget.googleMap.addListener('idle', function () {
-        var prevMv489 = 'undefined' === typeof __PS_MV_REG ? (__PS_MV_REG = undefined) : __PS_MV_REG;
+        var prevMv16 = 'undefined' === typeof __PS_MV_REG ? (__PS_MV_REG = undefined) : __PS_MV_REG;
         try {
             var south = decodeBounds(MAP.googleMap.getBounds());
-            var _db490 = decodeBounds === __PS_MV_REG['tag'] ? __PS_MV_REG['values'] : [];
-            var west = _db490[0];
-            var north = _db490[1];
-            var east = _db490[2];
+            var _db17 = decodeBounds === __PS_MV_REG['tag'] ? __PS_MV_REG['values'] : [];
+            var west = _db17[0];
+            var north = _db17[1];
+            var east = _db17[2];
             return sitesPopulate(SITES, south, west, north, east);
         } finally {
-            __PS_MV_REG = prevMv489;
+            __PS_MV_REG = prevMv16;
         };
     });
 };
 function geolocationSuccess(mapWidget, position) {
     var lat = position.coords.latitude;
     var lng = position.coords.longitude;
-    var position491 = new google.maps.LatLng({ 'lat' : lat, 'lng' : lng });
-    mapWidget.geolocationMarker.setPosition(position491);
-    return console.log('GEOLOCATION SUCCESS: lat=' + lat + ' lng=' + lng + ' postion=' + position491);
+    var position18 = new google.maps.LatLng({ 'lat' : lat, 'lng' : lng });
+    mapWidget.geolocationMarker.setPosition(position18);
+    return console.log('GEOLOCATION SUCCESS: lat=' + lat + ' lng=' + lng + ' postion=' + position18);
 };
 function geolocationError(positionError) {
     console.log('GEOLOCATION ERROR: ' + error.code + ': ' + error.message);
@@ -205,7 +214,7 @@ function initialize() {
         jQuery('#list-view').hide();
         return jQuery('#map-canvas').show();
     });
-    SITES = new Sites('http://127.0.0.1:4242/mhs-map/features.json');
+    SITES = new Sites(FEATURESJSONURI);
     LISTWIDGET = new ListWidget(SITES, jQuery('#list-view'));
     sitesSubscribeToPopulated(SITES, function () {
         console.log('LIST-WIDGET notified ' + SITES.sites.length);
